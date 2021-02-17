42cde707c5e86873cbfcc6820bdfa6210ce6d887
clay392 <2835237724@qq.com>
2021-02-17 10:58:39 +0800

    FirstHomeWork

---
+++ springboot-mybatis-tx_config/src/main/java/cn/tx/sboot/FirstSpringApplication.java
@@ -0,0 +1,12 @@
+package cn.tx.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@MapperScan("cn.tx.sboot.mapper")
+public class FirstSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringApplication.class, args);
+    }
+}
